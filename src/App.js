7535386e061316a1d006c8dc7563a0074f634260
Quy Cao <quy.caoquoc@gmail.com>
2021-11-12 22:31:13 +0700

    Show typing indicator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./App.css";
 
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 import "firebase/compat/storage";
 import "firebase/compat/auth";
+import "firebase/compat/database";
 
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollectionData } from "react-firebase-hooks/firestore";
@@ -14,6 +15,7 @@ import camera from "./camera.png";
 firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
@@ -24,7 +26,7 @@ firebase.initializeApp({
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 const storage = firebase.storage();
-
+const database = firebase.database();
 
 function App() {
   const [user] = useAuthState(auth);
@@ -75,13 +77,16 @@ function ChatRoom() {
   const [countValue, setCountValue] = useState(pageSize);
   const [user] = useAuthState(auth);
 
-  var messagesRef = firestore.collection("messages");
+  const messagesRef = firestore.collection("messages");
   var query = messagesRef.orderBy("createdAt").limitToLast(countValue);
   var [messages] = useCollectionData(query, { idField: "id" });
 
-  const sendMessage = async (e) => {
-    console.log(user.uid);
+  const typingByRef = database.ref("typing_by");
+  const [timestamp]= useState(Date.now());
+  const [typingBy, setTypingBy] = useState(0);
+  const [firstRender, setFirstRender] = useState(true);
 
+  const sendMessage = async (e) => {
     e.preventDefault();
 
     if (formValue.length === 0) { return; }
@@ -96,14 +101,17 @@ function ChatRoom() {
       uid: user.uid
     })
 
+    endTyping();
     dummy.current.scrollIntoView({ behavior: "smooth" });
   }
 
-  const handleImageAsFile = (e) => {
+  const onFileInputChange = (e) => {
     e.preventDefault();
 
-    const file = e.target.files[0]
+    const file = e.target.files[0];
     if (!file) { return; }
+
+    startTyping();
 
     const ref = storage.ref(`/images/${Date.now()}_${file.name}`);
     const uploadTask = ref.put(file);
@@ -116,32 +124,72 @@ function ChatRoom() {
             image_url: url,
             createdAt: firebase.firestore.FieldValue.serverTimestamp(),
             uid: user.uid
-          })
+          });
 
           dummy.current.scrollIntoView({ behavior: "smooth" });
           e.target.value = null;
+          endTyping();
         });
     });
   }
 
-  const handleLoadMore = (e) => {
+  const loadOlderMessages = (e) => {
     e.preventDefault();
-
     setCountValue(countValue + pageSize);
   }
 
+  const onTextInputChange = (e) => {
+    setFormValue(e.target.value);
+
+    if (e.target.value) {
+      startTyping();
+    } else {
+      endTyping();
+    }
+  }
+
+  const startTyping = () => {
+    typingByRef.set(timestamp);
+  }
+
+  const endTyping = () => {
+    if (typingBy !== timestamp) { return; }
+
+    typingByRef.set(0);
+  }
+
+  const isSomeoneTyping = () => {
+    return typingBy > 0 && typingBy !== timestamp;
+  }
+
+  useEffect(() => {
+    if (firstRender) {
+      setFirstRender(false);
+
+      startTyping();
+
+      typingByRef.on("value", (snapshot) => {
+        setTypingBy(snapshot.val());
+      }, (errorObject) => {
+        console.log("The read failed: " + errorObject.name);
+      });
+    }
+  });
+
   return (<>
-    <main>
-      {messages && messages.length >= pageSize && <button onClick={handleLoadMore}>Tải tin nhắn cũ hơn</button>}
+    <main className={isSomeoneTyping() ? "main_typing" : ""}>
+      {messages && messages.length >= pageSize && <button onClick={loadOlderMessages}>Tải tin nhắn cũ hơn</button>}
       {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
       <span ref={dummy}></span>
     </main>
 
+    {isSomeoneTyping() && <div className="typing_indicator"><p>Đối phương đang viết...</p></div>}
+
     <form onSubmit={sendMessage}>
-      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="Bạn đang nghĩ..." />
+      <input value={formValue} onChange={onTextInputChange} placeholder="Bạn đang nghĩ..." />
       <label>
         <img className="camera" src={camera} alt="" />
-        <input type="file" onChange={handleImageAsFile} />
+        <input type="file" onChange={onFileInputChange} />
       </label>
     </form>
   </>)
